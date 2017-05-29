d914de06f603c5fae3051ae1550d589977cedd85
sawak <sawak@DESKTOP-AE9MSO1>
2017-05-29 16:57:50 +0100

    login

--- Server/WindowsFormsApplication1/Form1.Designer.cs
+++ Server/WindowsFormsApplication1/Form1.Designer.cs
@@ -34,6 +34,12 @@
             this.btnLigacao = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // progressBar1
@@ -85,11 +91,69 @@
             this.button1.Text = "Imagem";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(340, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(340, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(405, 108);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(181, 20);
+            this.txtUsername.TabIndex = 8;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(405, 159);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(181, 20);
+            this.txtPassword.TabIndex = 9;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(405, 214);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(65, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(646, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnLigacao);
             this.Controls.Add(this.btnEnviarMensagem);
@@ -111,6 +175,12 @@
         private System.Windows.Forms.Button btnLigacao;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Server/WindowsFormsApplication1/Form1.cs
+++ Server/WindowsFormsApplication1/Form1.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -111,6 +113,214 @@ namespace WindowsFormsApplication1
             }
             //Console.ReadKey();
         }
+
+        private void WriteStatus(string msg)
+        {
+            
+        }
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            /*byte[] username = Encoding.UTF8.GetBytes(txtUsername.Text);
+
+            byte[] password = Encoding.UTF8.GetBytes(txtPassword.Text);
+
+            networkStream.Write(username, 0, username.Length);
+
+            networkStream.Write(password, 0, password.Length);
+
+
+            byte[] loginResponse = new byte[2];
+            networkStream.Read(loginResponse, 0, 2);
+            string response = Encoding.UTF8.GetString(loginResponse);
+            */
+
+
+            string username = txtUsername.Text;
+
+            string password = txtPassword.Text;
+
+
+            if (VerifyLogin(username, password))
+            {
+                MessageBox.Show("Valid user");
+            }
+
+            else
+            {
+                MessageBox.Show("Invalid User");
+            }
+
+        }
+
+        private void Register(string username, byte[] saltedPasswordHash, byte[] salt)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                // Configurar ligação à Base de Dados
+                conn = new SqlConnection();
+                conn.ConnectionString = String.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\sawak\Source\Repos\Projeto-TS\Server\WindowsFormsApplication1\DatabaseT_Seg.mdf;Integrated Security=True");
+
+                // Abrir ligação à Base de Dados
+                conn.Open();
+
+                // Declaração dos parâmetros do comando SQL
+                SqlParameter paramUsername = new SqlParameter("@username", username);
+                SqlParameter paramPassHash = new SqlParameter("@saltedPasswordHash", saltedPasswordHash);
+                SqlParameter paramSalt = new SqlParameter("@salt", salt);
+
+                // Declaração do comando SQL
+                String sql = "INSERT INTO Users (Username, SaltedPasswordHash, Salt) VALUES (@username,@saltedPasswordHash,@salt)";
+
+                // Prepara comando SQL para ser executado na Base de Dados
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                // Introduzir valores aos parâmentros registados no comando SQL
+                cmd.Parameters.Add(paramUsername);
+                cmd.Parameters.Add(paramPassHash);
+                cmd.Parameters.Add(paramSalt);
+
+                // Executar comando SQL
+                int lines = cmd.ExecuteNonQuery();
+
+                // Fechar ligação
+                conn.Close();
+                if (lines == 0)
+                {
+                    // Se forem devolvidas 0 linhas alteradas então o não foi executado com sucesso
+                    throw new Exception("Error while inserting an user");
+                }
+
+                else
+                {
+                    MessageBox.Show("User inserted!");
+
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error while inserting an user:" + e.Message);
+            }
+        }
+
+        private static byte[] GenerateSalt(int size)
+        {
+            //Generate a cryptographic random number.
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buff = new byte[size];
+            rng.GetBytes(buff);
+            return buff;
+        }
+
+        private static byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
+        {
+            using (HashAlgorithm hashAlgorithm = SHA512.Create())
+            {
+                // Declarar e inicializar buffer para o texto e salt
+                byte[] plainTextWithSaltBytes =
+                              new byte[plainText.Length + salt.Length];
+
+                // Copiar texto para buffer
+                for (int i = 0; i < plainText.Length; i++)
+                {
+                    plainTextWithSaltBytes[i] = plainText[i];
+                }
+                // Copiar salt para buffer a seguir ao texto
+                for (int i = 0; i < salt.Length; i++)
+                {
+                    plainTextWithSaltBytes[plainText.Length + i] = salt[i];
+                }
+
+                //Devolver hash do text + salt
+                return hashAlgorithm.ComputeHash(plainTextWithSaltBytes);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] salt = GenerateSalt(8);
+
+            //  textBoxSalt.Text = Convert.ToBase64String(salt);
+
+            //  textBoxSizeSalt.Text = (salt.Length * 8).ToString();
+
+            byte[] pass = Encoding.UTF8.GetBytes("123"); //não buscar nas txt
+
+            byte[] hash = GenerateSaltedHash(pass, salt);
+
+            //textBoxSaltedHash.Text = Convert.ToBase64String(hash);
+
+            // textBoxSizePass.Text = (hash.Length * 8).ToString();
+
+            string username = "adm";//textBoxUsername.Text;
+
+            Register(username, hash, salt);
+        }
+
+        private bool VerifyLogin(string username, string password)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                // Configurar ligação à Base de Dados
+                conn = new SqlConnection();
+                conn.ConnectionString = String.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\sawak\Desktop\Ipleiria\2º Semestre\Tópicos de Segurança\Ficha08\FichaPratica8_Base\Database1.mdf';Integrated Security=True");
+
+                // Abrir ligação à Base de Dados
+                conn.Open();
+
+                // Declaração do comando SQL
+                String sql = "SELECT * FROM Users WHERE Username = @username";
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = sql;
+
+                // Declaração dos parâmetros do comando SQL
+                SqlParameter param = new SqlParameter("@username", username);
+
+                // Introduzir valor ao parâmentro registado no comando SQL
+                cmd.Parameters.Add(param);
+
+                // Associar ligação à Base de Dados ao comando a ser executado
+                cmd.Connection = conn;
+
+                // Executar comando SQL
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    throw new Exception("Error while trying to access an user");
+                }
+
+                // Ler resultado da pesquisa
+                reader.Read();
+
+                // Obter Hash (password + salt)
+                byte[] saltedPasswordHashStored = (byte[])reader["SaltedPasswordHash"];
+
+                // Obter salt
+                byte[] saltStored = (byte[])reader["Salt"];
+
+                conn.Close();
+
+
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+                byte[] hash = GenerateSaltedHash(passwordBytes, saltStored);
+
+                return saltedPasswordHashStored.SequenceEqual(hash);
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("An error occurred");
+                return false;
+            }
+        }
+
     }
 
        
